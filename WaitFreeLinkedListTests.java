e4b2d1a20b200508e6fb5f3f2701a6b63d7cccda
Robert Hernandez <killerjaguar26@gmail.com>
2016-02-15 17:34:18 -0500

    Created tests for 1, 2, 4, and 8 threads

--- WaitFreeLinkedListTests.java
+++ WaitFreeLinkedListTests.java
@@ -1,44 +1,266 @@
+import java.util.Random;
+
 public class WaitFreeLinkedListTests
 {
+	static final int NUM_TESTS = 500000;
+	static final int RAND_MAX = 1000000;
+	
 	public static void main(String[] args)
-	{
+	{		
 		WaitFreeLinkedList<Integer> test;
+		
 		test = new SequentialWaitFreeLinkedList<Integer>();
 		
-		// Insert
+		System.out.println("Testing one thread...");
+		System.out.println("Execution time: " + test1Thread(test) + " ms");
 		
-		test.insert(5);
-		test.insert(3);
-		test.insert(10);
-		test.insert(13);
-		test.insert(1);
-		test.insert(-1);
-		test.insert(6);
-		test.insert(18);
+		test = new SequentialWaitFreeLinkedList<Integer>();
 		
-		System.out.println(test.toString());
-
-		// Insert duplicate
+		try
+		{
+			System.out.println("Testing two threads...");
+			System.out.println("Execution time: " + test2Threads(test) + " ms");
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println(e.getMessage());
+		}
+			
+		test = new SequentialWaitFreeLinkedList<Integer>();
 		
-		test.insert(10);
-		test.insert(5);
+		try
+		{
+			System.out.println("Testing four threads...");
+			System.out.println("Execution time: " + test4Threads(test) + " ms");
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println(e.getMessage());
+		}
 		
-		System.out.println(test.toString());
+		test = new SequentialWaitFreeLinkedList<Integer>();
 		
-		// Delete
+		try
+		{
+			System.out.println("Testing eight threads...");
+			System.out.println("Execution time: " + test8Threads(test) + " ms");
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	static long test1Thread(WaitFreeLinkedList<Integer> test)
+	{
+		long startTime = System.currentTimeMillis();
 		
-		test.delete(-1);
-		test.delete(19);
-		test.delete(13);
-		test.delete(5);
+		testInsert(test, NUM_TESTS/2);
+		testDelete(test, NUM_TESTS/2);
 		
-		System.out.println(test.toString());
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	static long test2Threads(WaitFreeLinkedList<Integer> test)
+		throws InterruptedException
+	{
+		long startTime = System.currentTimeMillis();
 		
-		// Contains
+		Thread[] threads = new Thread[2];
 		
-		System.out.println("test.contains(5) = " + test.contains(5));
-		System.out.println("test.contains(-1) = " + test.contains(-1));
-		System.out.println("test.contains(6) = " + test.contains(6));
-		System.out.println("test.contains(18) = " + test.contains(18));
+		// First thread inserts
+		threads[0] = new Thread()
+		{
+			public void run()
+			{
+				testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[0].start();
+		
+		// Second thread deletes
+		threads[1] = new Thread()
+		{
+			public void run()
+			{
+				testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[1].start();
+		
+		for (Thread t : threads)
+			t.join();
+		
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	static long test4Threads(WaitFreeLinkedList<Integer> test)
+		throws InterruptedException
+	{
+		long startTime = System.currentTimeMillis();
+		
+		Thread[] threads = new Thread[4];
+		
+		threads[0] = new Thread()
+		{
+			public void run()
+			{
+				testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[0].start();
+		
+		threads[1] = new Thread()
+		{
+			public void run()
+			{
+				testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[1].start();
+		
+		threads[2] = new Thread()
+		{
+			public void run()
+			{
+				testDelete(test, NUM_TESTS/2);
+				testInsert(test, NUM_TESTS/2);
+			}
+		};
+		
+		threads[2].start();
+		
+		threads[3] = new Thread()
+		{
+			public void run()
+			{
+				testDelete(test, NUM_TESTS/4);
+				testInsert(test, NUM_TESTS/4);
+				testDelete(test, NUM_TESTS/4);
+				testInsert(test, NUM_TESTS/4);
+			}
+		};
+		
+		threads[3].start();
+		
+		for (Thread t : threads)
+			t.join();
+		
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	static long test8Threads(WaitFreeLinkedList<Integer> test)
+		throws InterruptedException
+	{
+		long startTime = System.currentTimeMillis();
+	
+		Thread[] threads = new Thread[8];
+		
+		threads[0] = new Thread()
+		{
+			public void run()
+			{
+				testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[0].start();
+		
+		threads[1] = new Thread()
+		{
+			public void run()
+			{
+				testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[1].start();
+		
+		threads[2] = new Thread()
+		{
+			public void run()
+			{
+				testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[2].start();
+		
+		threads[3] = new Thread()
+		{
+			public void run()
+			{
+				testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[3].start();
+		
+		threads[4] = new Thread()
+		{
+			public void run()
+			{
+				testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[4].start();
+		
+		threads[5] = new Thread()
+		{
+			public void run()
+			{
+				testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[5].start();
+		
+		threads[6] = new Thread()
+		{
+			public void run()
+			{
+				testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[6].start();
+		
+		threads[7] = new Thread()
+		{
+			public void run()
+			{
+				testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[7].start();
+		
+		for (Thread t : threads)
+			t.join();
+		
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	// Inserts random values n times
+	static void testInsert(WaitFreeLinkedList<Integer> tests, int n)
+	{
+		Random random = new Random();
+		
+		for (int i = 0; i < n; i++)
+			tests.insert(random.nextInt(RAND_MAX));
+	}
+	
+	// Deletes n random values
+	static void testDelete(WaitFreeLinkedList<Integer> tests, int n)
+	{
+		Random random = new Random();
+		
+		for (int i = 0; i < n; i++)
+			tests.delete(random.nextInt(RAND_MAX));
 	}
 }
