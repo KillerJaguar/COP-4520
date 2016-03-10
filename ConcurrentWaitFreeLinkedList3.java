5192715d991b69736f2a0b5eb14c4f6a40154b8b
Bob <sadachbia@knights.ucf.edu>
2016-03-10 22:02:27 +0000

    testing insertions; unsuccessful
    line 293 causes a null pointer exception

---
+++ ConcurrentWaitFreeLinkedList3.java
@@ -0,0 +1,301 @@
+/*
+To achieve wait-freedom, a helping mechanism is used. The
+helping mechanism employs a special state array, with an entry for
+each thread. When a thread wishes to perform an operation on the
+list, it first chooses a phase number, higher than all phase numbers
+previously selected, and posts an operation-descriptor in its state
+array entry. The operation descriptor describes the operation it
+wishes to perform and also contains the phase number. Next, the
+threadgoes throughall thestatearray, andhelps performoperations
+with smaller or equal phase numbers. This ensures wait-freedom:
+a delayed operation eventually receives help from all threads and
+soon completes.
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+public class ConcurrentWaitFreeLinkedList3<E>
+	//implements WaitFreeLinkedList<E>
+{
+	class Node
+	{
+		final E item;
+		final AtomicMarkableReference<Node> next;
+		
+		public Node(E item, Node next)
+		{
+			this.item = item;
+			this.next = new AtomicMarkableReference<Node>(next, false);
+		}
+	}
+	
+	class State
+	{
+		final long phaseNumber;
+		final Operation operation;
+		final E item;
+		
+		final AtomicBoolean success;
+		
+		public State(long phaseNumber, Operation operation, E item)
+		{
+			this.phaseNumber = phaseNumber;
+			this.operation = operation;
+			this.item = item;
+			this.success = new AtomicBoolean(false);
+		}
+	}
+	
+	class Window
+	{
+		final Node pred, curr;
+		
+		public Window(Node pred, Node curr)
+		{
+			this.pred = pred;
+			this.curr = curr;
+		}
+	}
+	
+	enum Operation
+	{
+		INSERT,
+		DELETE,
+	}
+	
+	final Node head, tail;
+	
+	// ID of executing thread
+	//ThreadLocal<Integer> threadId;
+	//AtomicInteger nextId;
+	
+	// State array of operations for each thread
+	final AtomicReferenceArray<State>threadState;
+	
+	// Current highest phase number 
+	AtomicLong phaseNumber;
+	
+	@SuppressWarnings("unchecked")
+	public ConcurrentWaitFreeLinkedList3(int numThreads)
+	{
+		this.tail = new Node(null, null);
+		this.head = new Node(null, tail);
+		
+		//this.threadId = new ThreadLocal<Integer>();
+		//this.nextId = new AtomicInteger();
+		
+		this.threadState = new AtomicReferenceArray<State>(numThreads);
+		this.phaseNumber = new AtomicLong(0);
+	}
+	
+	public void insert(int threadIdHelping, E item)
+	{
+		operate(threadIdHelping, Operation.INSERT, item);
+	}
+	
+	public void delete(int threadIdHelping, E item)
+	{
+		operate(threadIdHelping, Operation.DELETE, item);
+	}
+	
+	// WAIT-FREE
+	public boolean contains(int threadIdHelping, E item)
+	{
+		int hash = item.hashCode(), hashIter;
+		Node iter = head.next.getReference();
+		
+		while (iter != null && (hashIter = iter.item.hashCode()) <= hash)
+		{
+			if (hash == hashIter && item.equals(iter.item))
+				return !iter.next.isMarked();
+		}
+		
+		return false;
+	}
+	
+	// WARNING not multi-threaded, only a debug function to ensure correctness
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder("[");
+		for (Node iter = head; iter != null; iter = iter.next.getReference())
+		{
+			if (iter.item!=null)
+				sb.append(iter.item.toString());
+			else
+				sb.append("null");
+			if (iter.next != null)
+			sb.append(", ");
+			
+				
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+	
+	/*int getThreadId()
+	{
+		Integer index = threadId.get();
+		if (index == null)
+			threadId.set(nextId.getAndIncrement());
+		return index;
+	}*/
+	
+	void operate(int threadIdHelping, Operation operation, E item)
+	{
+		long phaseNumber = this.phaseNumber.getAndIncrement();
+
+		
+		State st = new State(phaseNumber, operation, item);
+		threadState.set(threadIdHelping, st);
+		
+		for(int i = 0; i < threadState.length(); i ++){
+			State state = threadState.get(i);
+			// No state exists, skip
+			if (state == null)
+				continue;
+			
+			// Higher phase number, skip
+			if (state.phaseNumber > phaseNumber)
+				continue;
+			
+			// Operation already completed. skip
+			if (state.success.get())
+				continue;
+			
+			// Perform operation
+			switch (state.operation)
+			{
+				case INSERT: _insert(item, state.success); break;
+				case DELETE: _delete(item, state.success); break;
+			}
+		}
+		
+		
+	}
+	
+	// WAIT-FREE(?)
+	boolean _insert(E item, AtomicBoolean success)
+	{
+		while (!success.get())
+		{
+			Window window = find(item);
+			Node pred = window.pred, curr = window.curr;
+			
+			// Item already exists
+			if (item.hashCode() == curr.item.hashCode() && item.equals(curr.item))
+				return false;
+			
+			// Create new node
+			Node node = new Node(item, curr);
+			
+			// Install new node, else retry loop
+			if (!success.get() && pred.next.compareAndSet(curr, node, false, false))
+			{
+				success.compareAndSet(false, true);
+				return true;
+			}
+		}
+		
+		// The operation succeeded by another thread, therefore it is true 
+		// that the item was inserted
+		return true;
+	}
+	
+	// LOCK-FREE
+	boolean _delete(E item, AtomicBoolean success)
+	{
+		while (true)
+		{
+			Window window = find(item);
+			Node pred = window.pred, curr = window.curr;
+			
+			// Item is not found
+			if (item.hashCode() != curr.item.hashCode() || !item.equals(curr.item))
+				return false;
+			
+			Node succ = curr.next.getReference();
+			
+			// Try to mark node as deleted
+			// If it doesn't work, just retry, otherwise job is done
+			if (!curr.next.compareAndSet(succ, succ, false, true))
+				continue;
+			
+			// Try to advance reference 
+			// If we don't succeed, someone else did or will
+			pred.next.compareAndSet(curr, succ, false, false);
+			return true;
+		}
+		
+		// search_delete
+		
+		/*Node iter = head, prev = null;
+		while (iter != null)
+		{
+			if (item.compareTo(iter.item) == 0)
+				break;
+			
+			prev = iter;
+			iter = iter.next;
+		}
+		
+		if (iter == null)
+			return;
+		
+		// execute_delete
+		
+		if (!iter.deleted.get())
+		{
+			iter.deleted.set(true);
+			
+			if (prev != null)
+				prev.next = iter.next;
+			
+			else
+				head = head.next;
+		}*/
+	}
+	
+	Window find(E item)
+	{
+		boolean snip;
+		
+		// If list changes while traversed, start over
+		retry: while(true)
+		{
+			// Start looking from head
+			Node pred = head;
+			Node curr = pred.next.getReference();
+			
+			// Move down the list
+			while (true)
+			{
+				boolean[] marked = {false};
+				Node succ = curr.next.get(marked);
+				
+				while (marked[0])
+				{
+					// Try to snip out node
+					// If predecessor's next field changed, must retry whole traversal
+					if (!pred.next.compareAndSet(curr, succ, false, false))
+						continue retry;
+					
+					curr = succ;
+					succ = curr.next.get(marked);
+				}
+				
+				if (item.hashCode() >= succ.item.hashCode() || item.equals(succ.item))//this line can produce a null pointer exception
+					return new Window(pred, succ);
+				
+				pred = curr;
+				curr = succ;
+			}
+		}
+	}
+}
---
+++ cwfllTests2.java
@@ -0,0 +1,449 @@
+import java.util.Random;
+
+public class cwfllTests2
+{
+	static final int NUM_TESTS = 500000;
+	static final int RAND_MAX  = 1000;//250000;
+	static final boolean SIMPLE= true;
+	
+	public static void main(String[] args)
+	{		
+		ConcurrentWaitFreeLinkedList3<Integer> test;
+		
+		test = new ConcurrentWaitFreeLinkedList3<Integer>(1);
+		
+		System.out.println("Testing one thread...");
+		if (SIMPLE){
+			simple1(test);
+		}
+		else{
+			System.out.println("Execution time 10i/80d/10c: " + test1Thread(test,.1,.8) + " ms");
+			System.out.println(test);
+			System.out.println("Execution time 80i/10d/10c: " + test1Thread(test,.8,.1) + " ms");
+			System.out.println("Execution time 10i/10d/80c: " + test1Thread(test,.1,.1) + " ms");
+		}
+		test = new ConcurrentWaitFreeLinkedList3<Integer>(2);
+		
+		try
+		{
+			if (SIMPLE){
+				//simple2(test);
+			}else{
+				System.out.println("Testing two threads...");
+				System.out.println("Execution time 10i/80d/10c: " + test2Threads(test,.1,.8) + " ms");
+				System.out.println("Execution time 80i/10d/10c: " + test2Threads(test,.8,.1) + " ms");
+				System.out.println("Execution time 10i/10d/80c: " + test2Threads(test,.1,.1) + " ms");
+			}
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println(e.getMessage());
+		}
+			
+		test = new ConcurrentWaitFreeLinkedList3<Integer>(4);
+		
+		try
+		{
+			if (SIMPLE){
+				//simple4(test);
+			}else{
+				System.out.println("Testing four threads...");
+				System.out.println("Execution time 10i/80d/10c: " + test4Threads(test,.1,.8) + " ms");
+				System.out.println("Execution time 80i/10d/10c: " + test4Threads(test,.8,.1) + " ms");
+				System.out.println("Execution time 10i/10d/80c: " + test4Threads(test,.1,.1) + " ms");
+			}
+			
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		test = new ConcurrentWaitFreeLinkedList3<Integer>(8);
+		
+		try
+		{
+			if (SIMPLE){
+				//simple8(test);
+			}else{
+				System.out.println("Testing eight threads...");
+				System.out.println("Execution time 10i/80d/10c: " + test8Threads(test,.1,.8) + " ms");
+				System.out.println("Execution time 80i/10d/10c: " + test8Threads(test,.8,.1) + " ms");
+				System.out.println("Execution time 10i/10d/80c: " + test8Threads(test,.1,.1) + " ms");
+			}
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	static long test1Thread(ConcurrentWaitFreeLinkedList3 test,double dist1, double dist2)
+	{
+		long startTime = System.currentTimeMillis();
+		
+		for (int i=0;i<NUM_TESTS;i++){
+			double temp=Math.random();
+			if (temp<=dist1)
+				testInsert(test, 1,0);
+			else if (temp<=dist2)
+				testDelete(test, 1,0);
+			else
+				testContains(test, 1,0);
+		}
+		
+		//testInsert(test, NUM_TESTS/2);
+		//testDelete(test, NUM_TESTS/2);
+		
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	static long test2Threads(ConcurrentWaitFreeLinkedList3 test,double dist1, double dist2)
+		throws InterruptedException
+	{
+		long startTime = System.currentTimeMillis();
+		
+		Thread[] threads = new Thread[2];
+		
+		// First thread inserts
+		threads[0] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+			}
+				//testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[0].start();
+		
+		// Second thread deletes
+		threads[1] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[1].start();
+		
+		for (Thread t : threads)
+			t.join();
+		
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	static long test4Threads(ConcurrentWaitFreeLinkedList3 test,double dist1, double dist2)
+		throws InterruptedException
+	{
+		long startTime = System.currentTimeMillis();
+		
+		Thread[] threads = new Thread[4];
+		
+		threads[0] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[0].start();
+		
+		threads[1] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[1].start();
+		
+		threads[2] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testDelete(test, NUM_TESTS/2);
+				//testInsert(test, NUM_TESTS/2);
+			}
+		};
+		
+		threads[2].start();
+		
+		threads[3] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testDelete(test, NUM_TESTS/4);
+				//testInsert(test, NUM_TESTS/4);
+				//testDelete(test, NUM_TESTS/4);
+				//testInsert(test, NUM_TESTS/4);
+			}
+		};
+		
+		threads[3].start();
+		
+		for (Thread t : threads)
+			t.join();
+		
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	static long test8Threads(ConcurrentWaitFreeLinkedList3 test,double dist1, double dist2)
+		throws InterruptedException
+	{
+		long startTime = System.currentTimeMillis();
+	
+		Thread[] threads = new Thread[8];
+		
+		threads[0] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[0].start();
+		
+		threads[1] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[1].start();
+		
+		threads[2] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[2].start();
+		
+		threads[3] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testInsert(test, NUM_TESTS);
+			}
+		};
+		
+		threads[3].start();
+		
+		threads[4] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[4].start();
+		
+		threads[5] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[5].start();
+		
+		threads[6] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[6].start();
+		
+		threads[7] = new Thread()
+		{
+			public void run()
+			{
+				for (int i=0;i<NUM_TESTS/2;i++){
+					double temp=Math.random();
+					if (temp<=dist1)
+						testInsert(test, 1,0);
+					else if (temp<=dist2)
+						testDelete(test, 1,0);
+					else
+						testContains(test, 1,0);
+				}
+				//testDelete(test, NUM_TESTS);
+			}
+		};
+		
+		threads[7].start();
+		
+		for (Thread t : threads)
+			t.join();
+		
+		return System.currentTimeMillis() - startTime;
+	}
+	
+	static void simple1(ConcurrentWaitFreeLinkedList3 tests){
+		
+		tests.insert(0, 10);
+		tests.insert(0, 23);
+		//tests.delete(0, 23);
+		tests.insert(0, 17);
+		System.out.println(tests);
+	}
+	
+	// Inserts random values n times
+	static void testInsert(ConcurrentWaitFreeLinkedList3 tests, int n, int thr)
+	{
+		Random random = new Random();
+		
+		for (int i = 0; i < n; i++)
+			tests.insert(thr, random.nextInt(RAND_MAX));
+	}
+	
+	// Deletes n random values
+	static void testDelete(ConcurrentWaitFreeLinkedList3 tests, int n, int thr)
+	{
+		//testContains(tests, n, thr);
+		Random random = new Random();
+		
+		for (int i = 0; i < n; i++)
+			tests.delete(thr, random.nextInt(RAND_MAX));
+	}
+	
+	// checks for n random values
+	static void testContains(ConcurrentWaitFreeLinkedList3 tests, int n, int thr)
+	{
+		Random random = new Random();
+		
+		for (int i = 0; i < n; i++)
+			tests.contains(thr, random.nextInt(RAND_MAX));
+	}
+	
+}
